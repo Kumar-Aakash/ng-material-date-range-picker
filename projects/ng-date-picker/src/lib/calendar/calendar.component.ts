d003bea260459615fb6d49046effa64ccbccca24
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-09-13 20:25:26 +0530

    feat: added events and operations on button click.

--- projects/ng-date-picker/src/lib/calendar/calendar.component.ts
+++ projects/ng-date-picker/src/lib/calendar/calendar.component.ts
@@ -5,6 +5,7 @@
  */
 
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
@@ -23,7 +24,7 @@ import * as moment from 'moment';
   styleUrls: ['./calendar.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalendarComponent {
+export class CalendarComponent implements AfterViewInit {
   firstCalendarViewData!: CalendarViewData;
   secondCalendarViewData!: CalendarViewData;
   @Input() selectedDates!: DateRange<Date>;
@@ -31,9 +32,27 @@ export class CalendarComponent {
   @ViewChild('firstCalendarView') firstCalendarView!: MatCalendar<Date>;
   @ViewChild('secondCalendarView') secondCalendarView!: MatCalendar<Date>;
 
-  constructor(private cdref: ChangeDetectorRef) {
+  constructor(
+    private cdref: ChangeDetectorRef,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {
     this.initFirstCalendar();
     this.initSecondCalendar();
+  }
+
+  ngAfterViewInit(): void {
+    this.addFirstCalendarButtonEvents();
+  }
+
+  /**
+   * This method handles first calendar view month selection.
+   *
+   * @param event Date
+   */
+  monthSelected(event: Date) {
+    this.secondCalendarView._goToDateInView(event, 'year');
+    this.handleFirstCalendarNextEvent(this, true);
   }
 
   /**
@@ -46,13 +65,87 @@ export class CalendarComponent {
     if (
       !selectedDates ||
       (selectedDates.start && selectedDates.end) ||
-      moment(selectedDates.start).isAfter(date)
+      (selectedDates.start && date && selectedDates.start > date)
     ) {
       this.selectedDates = new DateRange<Date>(date, null);
     } else {
       this.selectedDates = new DateRange<Date>(selectedDates.start, date);
     }
     this.cdref.markForCheck();
+  }
+
+  /**
+   * This method handles First calendar prev button event.
+   * @param classRef CalendarComponent
+   */
+  private handleFirstCalDatePrevEvent(classRef: CalendarComponent): void {
+    const leftDateCalender = classRef.firstCalendarView;
+    if (leftDateCalender.currentView.toLocaleLowerCase() === 'month') {
+      const date: Date = new Date(leftDateCalender['_clampedActiveDate']);
+      classRef.secondCalendarView.minDate =
+        classRef.getFirstDateOfNextMonth(date);
+      classRef.cdref.markForCheck();
+    }
+  }
+
+  /**
+   * This method handle the next button event.
+   *
+   * @param classRef CalendarComponent
+   * @param isForced boolean
+   */
+  private handleFirstCalendarNextEvent(
+    classRef: CalendarComponent,
+    isForced = false
+  ): void {
+    const firstCalendar = classRef.firstCalendarView;
+    if (firstCalendar.currentView.toLocaleLowerCase() === 'month' || isForced) {
+      const date: Date = new Date(firstCalendar['_clampedActiveDate']);
+      const nextMonthDate = classRef.getFirstDateOfNextMonth(date);
+      classRef.secondCalendarView.minDate = nextMonthDate;
+      classRef.secondCalendarView._goToDateInView(nextMonthDate, 'month');
+      setTimeout(() => {
+        const btn: HTMLButtonElement[] =
+          classRef.el.nativeElement.querySelectorAll(
+            '#secondCalendarView button.mat-calendar-body-active'
+          );
+        if (btn?.length) {
+          btn[0].blur();
+        }
+      }, 1);
+      classRef.cdref.markForCheck();
+    }
+  }
+
+  /**
+   * This method attaches next and prev events on buttons.
+   *
+   */
+  private addFirstCalendarButtonEvents(): void {
+    const monthPrevBtn = this.el.nativeElement.querySelectorAll(
+      '#firstCalendarView .mat-calendar-previous-button'
+    );
+    const monthNextBtn = this.el.nativeElement.querySelectorAll(
+      '#firstCalendarView .mat-calendar-next-button'
+    );
+    this.attachClickEvent(monthPrevBtn, this.handleFirstCalDatePrevEvent);
+    this.attachClickEvent(monthNextBtn, this.handleFirstCalendarNextEvent);
+  }
+
+  /**
+   * This method attach the next and prev events on specified nodes.
+   *
+   * @param nodes any
+   * @param handler Function
+   */
+  private attachClickEvent(nodes: any, handler: Function): void {
+    if (nodes) {
+      Array.from(nodes).forEach((button) => {
+        this.renderer.listen(button, 'click', () => {
+          handler(this);
+        });
+      });
+    }
   }
 
   /**
